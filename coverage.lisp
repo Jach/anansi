30a70b7e4d4975659562563da64b0f52e27b8407
Kevin Secretan <git@thejach.com>
2025-09-15 11:16:10 -0700

    Back to normal coverage view. Would like to filter faulty defclass and related coverage..

--- coverage.lisp
+++ coverage.lisp
@@ -2,17 +2,17 @@
 Run tests + generate coverage report, use sbcl --script coverage.lisp
 |#
 (in-package #:cl-user)
-
-(defvar *system* "anansi")
-
-(require :sb-cover)
-(declaim (optimize sb-cover:store-coverage-data))
-
 #-quicklisp
 (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
                                        (user-homedir-pathname))))
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
+
+
+(defvar *system* "anansi")
+
+(require :sb-cover)
+(declaim (optimize sb-cover:store-coverage-data))
 
 (uiop:delete-directory-tree #p"coverage/" :validate t :if-does-not-exist :ignore)
 
@@ -21,4 +21,4 @@ Run tests + generate coverage report, use sbcl --script coverage.lisp
         (*load-verbose* nil))
     (asdf:load-system *system* :force t)
     (asdf:test-system *system*))
-  (sb-cover:report "coverage/" :form-mode :car))
+  (sb-cover:report "coverage/"))
